c5f3ad9e0fcb356028d8a1b7484574c1eb032b24
Mikhail Stolovich <stolovich@yahoo.com>
2015-03-03 00:34:04 +0200

    Project created background added player movement and player itself added

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed;
+    public float angle;
+    public Boundary boundary;
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        if ((moveHorizontal == 0) && (moveVertical!=0))
+        {
+            if(Input.GetKey(KeyCode.W))
+            {
+                transform.Translate(Vector3.back * speed);
+            }
+            else
+            {
+                if (Input.GetKey(KeyCode.S))
+                {
+                    transform.Translate(Vector3.forward * speed);
+                }
+            }
+        }
+        else
+        {
+
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Rotate(Vector3.up, angle);
+            }
+            else
+            {
+                if (Input.GetKey(KeyCode.A))
+                {
+                    transform.Rotate(Vector3.down, angle);
+                }
+            }
+            rigidbody.position = new Vector3
+            (
+               Mathf.Clamp(rigidbody.position.x, boundary.xMin, boundary.xMax),
+               0.0f,
+               Mathf.Clamp(rigidbody.position.z, boundary.zMin, boundary.zMax)
+            );
+        }
+    }
+}
